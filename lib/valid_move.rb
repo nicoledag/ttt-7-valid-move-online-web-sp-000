026f7383487c47d0fc3b26f7602429a5baf68f2f
Nicole D <nnardella@gmail.com>
2018-10-01 00:44:00 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,10 +1,15 @@
 def valid_move?(board, index)
-  if board[index] == " "
-    return true
-  else
-    return false
-  end
+
+if index.between?(0, 8) && !position_taken?(board, index)
+
+  return true
+else
+ return false
+ end
 end
+
+
+
 
 
 
